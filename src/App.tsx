93b653322543f75ad72753d0ec25c80349a3bdf3
lighthouse-dev <lighthouse2.dev@gmail.com>
2020-12-20 23:35:47 +0900

    AppとMainコンポーネントを結合

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from "react";
-import "./App.css";
-import TopBar from "./components/TopBar";
-import Main from "./pages/Main";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 
-function App() {
+import TopBar from './components/TopBar';
+import Login from './pages/Login';
+import RecipeList from './pages/RecipeList';
+import AddRecipe from './pages/AddRecipe';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      padding: theme.spacing(1)
+    }
+  })
+);
+
+const App = () => {
+  const classes = useStyles();
   return (
     <>
-      <TopBar />
-      <Main />
+      <Router>
+        {/* <Link to="/add">add</Link> */}
+
+        <Switch>
+          <Route exact path="/" component={Login}></Route>
+          <>
+            <TopBar />
+            <div className={classes.main}>
+              <Route exact path="/list" component={RecipeList}></Route>
+              <Route path="/add" component={AddRecipe}></Route>
+            </div>
+          </>
+        </Switch>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
